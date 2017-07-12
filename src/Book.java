f1be4a4e80a00cd6f7dfe01bcbf688413ea20acd
mmhayman <mmhayman@svsu.edu>
2017-07-12 14:02:55 -0400

    added book class

---
+++ src/Book.java
@@ -0,0 +1,60 @@
+import java.util.Date;
+
+/**
+ * Created by michelhayman on 7/12/17.
+ */
+public class Book {
+
+    private String title;
+    private String author;
+    private boolean status;
+    private Date dueDate;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+    public Book (String title, String author, boolean status, Date dueDate) {
+        this.title = title;
+        this.author = author;
+        this.status = status;
+        this.dueDate = dueDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", status=" + status +
+                ", dueDate=" + dueDate +
+                '}';
+    }
+}
