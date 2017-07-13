369ff05de164c276adebbdda16141f9f177afafa
Richard Ren <rren1952@gmail.com>
2017-07-13 14:55:30 -0400

    librarymain v0re0 bar
    yum.

--- src/LibraryMain.java
+++ src/LibraryMain.java
@@ -11,30 +11,32 @@ public class LibraryMain {
 
     //assuming Book(title, author, boolean, LocalDate)
     public static void main(String[] args) {
-        // display()
-        // addBook()
-        // searchAuthor()
-        // searchTitleKeyword()
-        // checkoutBook()
-        // returnBook()
+        Scanner scan = new Scanner(System.in);
+
+        ArrayList<Book> books = new ArrayList<Book>();
+        books.add(new Book("Billy's Adventures", "Bully Billy", true, LocalDate.now()));
+        books.add(new Book("Anna's Adventures", "Astral Anna", false, LocalDate.now()));
+        checkoutBook(books.get(1));
+        checkoutBook(books.get(0));
+        display(books, "");
     }
 
-    public void searchTitleKeyword(ArrayList<Book> books, String keyword) {
+    public static void searchTitleKeyword(ArrayList<Book> books, String keyword) {
         System.out.println("The books with that keyword in their title:");
         for (int i = 0; i < books.size(); i++) {
-            if (books.get(i).getTitle().indexOf(keyword) != -1) {
-                System.out.println((i+1) + "- " + books.get(i).getTitle() + " by" + books.get(i).getAuthor());
+            if (books.get(i).getTitle().toLowerCase().contains(keyword.toLowerCase())) {
+                System.out.println((i+1) + " - " + books.get(i).getTitle() + " by " + books.get(i).getAuthor());
             }
         }
         System.out.println();
         System.out.println("Anything else?");
     }
 
-    public void searchAuthor(ArrayList<Book> books, String author) {
+    public static void searchAuthor(ArrayList<Book> books, String author) {
         System.out.println("The books made by that author:");
         for (int i = 0; i < books.size(); i++) {
-            if (books.get(i).getAuthor().indexOf(author) != -1) {
-                System.out.println((i+1) + "- " + books.get(i).getTitle() + " by" + books.get(i).getAuthor());
+            if (books.get(i).getAuthor().toLowerCase().contains(author.toLowerCase())) {
+                System.out.println((i+1) + " - " + books.get(i).getTitle() + " by " + books.get(i).getAuthor());
             }
         }
         System.out.println();
@@ -42,14 +44,19 @@ public class LibraryMain {
     }
 
 
-    public void display(ArrayList<Book> books, String status){
-        System.out.println("Here's a list of all of our " + status + "books:");
+    public static void display(ArrayList<Book> books, String status){
+        System.out.println("Here's a list of all of our " + status + " books:");
         for (int i = 0; i < books.size(); i++) {
-            System.out.println((i+1) + " - " + books.get(i));
+            System.out.print((i+1) + " - " + books.get(i).getTitle() + " by " + books.get(i).getAuthor() + ", ");
+            if(books.get(i).isStatus() == true) {
+                System.out.println("currently on-shelf.");
+            } else {
+                System.out.println("currently checked out. Due date is: " + books.get(i).getDueDate());
+            }
         }
     }
 
-    public void addBook(Scanner scan, ArrayList<Book> books) {
+    public static void addBook(Scanner scan, ArrayList<Book> books) {
         System.out.println("Alright, a new book. What's the title?");
         String title = scan.nextLine();
         System.out.println("Who was the Author?");
@@ -60,7 +67,7 @@ public class LibraryMain {
     }
 
     //checking out a book
-    public void checkoutBook(Book book) {
+    public static void checkoutBook(Book book) {
         // if book status is false, then it's checked out.
         if (!book.isStatus())
             System.out.println("That book's currently checked out. It's due to be back at " + book.getDueDate());
@@ -72,7 +79,7 @@ public class LibraryMain {
     }
 
     // returning a book.
-    public void returnBook(Book book) {
+    public static void returnBook(Book book) {
         // if book status is true, then it's on shelf.
         if (book.isStatus())
             System.out.println("That book is already on shelf in our library!");
